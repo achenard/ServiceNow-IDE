cd31b25d62da8afe2195c129468732e1a1429a50
System Administrator <admin@example.com>
2026-02-04 13:37:38 -0500

    First update

---
+++ src/fluent/generated/data/form-section/sys_ui_section_cf78b2e22f76be101e83cb2cbfa4e364.now.ts
@@ -0,0 +1,14 @@
+import { Record } from '@servicenow/sdk/core'
+
+Record({
+    $id: Now.ID['cf78b2e22f76be101e83cb2cbfa4e364'],
+    table: 'sys_ui_section',
+    data: {
+        header: false,
+        name: 'x_snc_hrc_change_2_drrb',
+        sys_domain: 'global',
+        sys_domain_path: '/',
+        title: true,
+        view: 'Default view',
+    },
+})
---
+++ src/fluent/generated/other/sys-ui-element/sys_ui_section_cf78b2e22f76be101e83cb2cbfa4e364.now.ts
@@ -0,0 +1,206 @@
+import { Record } from '@servicenow/sdk/core'
+
+Record({
+    $id: Now.ID['4378b2e22f76be101e83cb2cbfa4e36c'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'number',
+        position: 0,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+    },
+})
+Record({
+    $id: Now.ID['0778b2e22f76be101e83cb2cbfa4e36d'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'short_description',
+        position: 1,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+    },
+})
+Record({
+    $id: Now.ID['8378b2e22f76be101e83cb2cbfa4e36e'],
+    table: 'sys_ui_element',
+    data: {
+        element: '.begin_split',
+        position: 2,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+        type: '.begin_split',
+    },
+})
+Record({
+    $id: Now.ID['0378b2e22f76be101e83cb2cbfa4e36f'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'state',
+        position: 3,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+    },
+})
+Record({
+    $id: Now.ID['8f78b2e22f76be101e83cb2cbfa4e36f'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'application_system',
+        position: 4,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+    },
+})
+Record({
+    $id: Now.ID['0f78b2e22f76be101e83cb2cbfa4e370'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'directorate',
+        position: 5,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+    },
+})
+Record({
+    $id: Now.ID['cf78b2e22f76be101e83cb2cbfa4e396'],
+    table: 'sys_ui_element',
+    data: {
+        element: '.split',
+        position: 6,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+        type: '.split',
+    },
+})
+Record({
+    $id: Now.ID['4f78b2e22f76be101e83cb2cbfa4e397'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'priority',
+        position: 7,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+    },
+})
+Record({
+    $id: Now.ID['cb78b2e22f76be101e83cb2cbfa4e398'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'submitter',
+        position: 8,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+    },
+})
+Record({
+    $id: Now.ID['4b78b2e22f76be101e83cb2cbfa4e399'],
+    table: 'sys_ui_element',
+    data: {
+        element: '.end_split',
+        position: 9,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+        type: '.end_split',
+    },
+})
+Record({
+    $id: Now.ID['c778b2e22f76be101e83cb2cbfa4e39a'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'description',
+        position: 10,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+    },
+})
+Record({
+    $id: Now.ID['4778b2e22f76be101e83cb2cbfa4e39b'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'division_assignment',
+        position: 11,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+    },
+})
+Record({
+    $id: Now.ID['c378b2e22f76be101e83cb2cbfa4e39c'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'functional_director',
+        position: 12,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+    },
+})
+Record({
+    $id: Now.ID['4378b2e22f76be101e83cb2cbfa4e39d'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'branch',
+        position: 13,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+    },
+})
+Record({
+    $id: Now.ID['cf78b2e22f76be101e83cb2cbfa4e39d'],
+    table: 'sys_ui_element',
+    data: {
+        element: '.begin_split',
+        position: 14,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+        type: '.begin_split',
+    },
+})
+Record({
+    $id: Now.ID['4f78b2e22f76be101e83cb2cbfa4e39e'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'active',
+        position: 15,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+    },
+})
+Record({
+    $id: Now.ID['cb78b2e22f76be101e83cb2cbfa4e39f'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'opened',
+        position: 16,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+    },
+})
+Record({
+    $id: Now.ID['4b78b2e22f76be101e83cb2cbfa4e3a0'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'assigned_to',
+        position: 17,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+    },
+})
+Record({
+    $id: Now.ID['c778b2e22f76be101e83cb2cbfa4e3a1'],
+    table: 'sys_ui_element',
+    data: {
+        element: '.split',
+        position: 18,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+        type: '.split',
+    },
+})
+Record({
+    $id: Now.ID['4778b2e22f76be101e83cb2cbfa4e3a2'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'opened_by',
+        position: 19,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+    },
+})
+Record({
+    $id: Now.ID['0b78b2e22f76be101e83cb2cbfa4e3c8'],
+    table: 'sys_ui_element',
+    data: {
+        element: 'updated',
+        position: 20,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+    },
+})
+Record({
+    $id: Now.ID['8778b2e22f76be101e83cb2cbfa4e3c9'],
+    table: 'sys_ui_element',
+    data: {
+        element: '.end_split',
+        position: 21,
+        sys_ui_section: 'cf78b2e22f76be101e83cb2cbfa4e364',
+        type: '.end_split',
+    },
+})
